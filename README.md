2a586485893827f09df6436dd2998f986dd96451
suyash <suyashambule1234@gmail.com>
2025-01-15 17:44:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Blog generation using AWS lambda
---
+++ app.py
@@ -0,0 +1,10 @@
+import boto3
+import botocore.config
+
+def Blog_generate_using_bedrock(blogtopc:str)->str:
+    prompts=f"
+    
+    
+    
+    "
+
